faf03edb8f852b158884aff84468d91eb9c3ee1c
Recep Pazarlı <receppazarli99@hotmail.com>
2021-05-25 09:20:25 +0300

    5. Ders Sonu

---
+++ InterfaceAbstractDemo1/Adapters/MernisServiceAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Entities;
+using InterfaceAbstractDemo1.MernisServiceReference;
+
+namespace InterfaceAbstractDemo1.Adapters
+{
+    public class MernisServiceAdapter :ICustomerCheckService
+    {
+        public bool CheckIfRealPerson(Customer customer)
+        {
+            KPSPublicSoapClient client = new KPSPublicSoapClient();
+            return client.TCKimlikNoDogrula(Convert.ToInt64(customer.NationalityId),
+                customer.FirstName.ToUpper(), customer.LastName.ToUpper(),
+                customer.DateOfBirth.Year);
+
+        }
+    }
+}
---
+++ InterfaceAbstractDemo1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using InterfaceAbstractDemo.Abstract;
+using InterfaceAbstractDemo.Concrete;
+using InterfaceAbstractDemo.Entities;
+using InterfaceAbstractDemo1.Adapters;
+
+namespace InterfaceAbstractDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            BaseCustomerManager customerManager = new NeroCustomerManager(new MernisServiceAdapter());
+            customerManager.Save(new Customer
+            {
+                DateOfBirth = new DateTime(1999, 11, 23),
+                FirstName = "Recep",
+                LastName = "Pazarlı",
+                NationalityId = "22450529986"
+
+            });
+            Console.ReadLine();
+
+        }
+    }
+}
